4cca5355fe5073ad3a1d75240b15f24e561b9b50
Lindsay Block <lindsay.block05@gmail.com>
2018-04-17 12:02:02 -0600

    payments

--- client/src/components/BraintreeDrop.js
+++ client/src/components/BraintreeDrop.js
@@ -7,13 +7,19 @@ import {
 import axios from 'axios';
 import { setFlash } from '../actions/flash';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { setHeaders } from '../actions/headers';
 import braintree from 'braintree-web-drop-in';
 import BraintreeDropin from 'braintree-dropin-react';
 import BraintreeSubmitButton from './BraintreeSubmitButton';
 
 class BraintreeDrop extends React.Component {
-  state = { loaded: false, token: '' }
+  state = {
+    loaded: false,
+    token: '',
+    redirect: false,
+    transactionId: '',
+  }
 
   componentDidMount() {
     const { dispatch } = this.props;
@@ -30,11 +36,34 @@ class BraintreeDrop extends React.Component {
   }
 
   handlePaymentMethod = (payload) => {
-    debugger
+    const { dispatch, amount } = this.props;
+    axios.post('/api/payment', { amount, ...payload} )
+      .then( ({ data: transactionId, headers }) => {
+        dispatch(setHeaders(headers));
+        this.setState({ transactionId, redirect: true })
+      })
   }
 
   render() {
-    const { loaded, token } = this.state;
+    const {
+      loaded,
+      token,
+      redirect,
+      transactionId,
+    } = this.state;
+
+    if (redirect)
+      return (
+        <Redirect to={{
+          pathname: '/payment_success',
+          state: {
+            amount: this.props.amount,
+            transactionId
+          }
+        }}/>
+
+      )
+
 
     if (loaded)
       return (
---
+++ client/src/components/PaymentSuccess.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Segment, Header } from 'semantic-ui-react';
+import { Link, Redirect } from 'react-router-dom';
+
+const PaymentSuccess = ({ location: { state }}) => {
+  if (state && state.transactionId)
+    return(
+      <Segment basit textAlign="center">
+        <Header as="h1" color="green">
+          Thank you for shopping
+        </Header>
+        <p> You have been charged ${state.amount}</p>
+        <p>Your transaction ID is: {state.transactionId}</p>
+        <Link to='/'>Buy more stuff</Link>
+      </Segment>
+    )
+    else
+      return <Redirect to="/" />
+}
+
+export default PaymentSuccess;
